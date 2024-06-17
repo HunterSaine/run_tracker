4e1395c196bb1d7ec17485535ebd9d1e3202187f
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-17 19:39:02 -0400

    initial commit

---
+++ src/test/java/dev/hunter/runnerz/RunnerzApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.hunter.runnerz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RunnerzApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
