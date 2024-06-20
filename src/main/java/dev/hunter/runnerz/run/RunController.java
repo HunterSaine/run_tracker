72c675e3476f157d19edd6729d4c72cd26eab5a2
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-20 18:32:15 -0400

    deleted code from run repository and created a JdbcClient

--- src/main/java/dev/hunter/runnerz/run/RunController.java
+++ src/main/java/dev/hunter/runnerz/run/RunController.java
@@ -8,30 +8,30 @@ import java.util.Optional;
 @RestController
 public class RunController {
 
-    private final RunRepository runRepository;
-
-    public RunController(RunRepository runRepository) {
-        this.runRepository = runRepository;
-    }
-
-    @GetMapping("/runs")
-    List<Run> findAll(){
-        return runRepository.findAll();
-    }
-    @GetMapping("/runs/{id}")
-    Optional<Run> findByRunId(@PathVariable Integer id){
-        return runRepository.findByRunId(id);
-    }
-    @PostMapping("/runs")
-    void newRun(@RequestBody Run run){
-        runRepository.create(run);
-    }
-    @PutMapping("/runs/{id}")
-    void updateRun(@RequestBody Run run,@PathVariable Integer id){
-        runRepository.update(run,id);
-    }
-    @DeleteMapping("/runs/delete/{id}")
-    void deleteRun(@RequestBody @PathVariable Integer id){
-        runRepository.delete(id);
-    }
+//    private final RunRepository runRepository;
+//
+//    public RunController(RunRepository runRepository) {
+//        this.runRepository = runRepository;
+//    }
+//
+//    @GetMapping("/runs")
+//    List<Run> findAll(){
+//        return runRepository.findAll();
+//    }
+//    @GetMapping("/runs/{id}")
+//    Optional<Run> findByRunId(@PathVariable Integer id){
+//        return runRepository.findByRunId(id);
+//    }
+//    @PostMapping("/runs")
+//    void newRun(@RequestBody Run run){
+//        runRepository.create(run);
+//    }
+//    @PutMapping("/runs/{id}")
+//    void updateRun(@RequestBody Run run,@PathVariable Integer id){
+//        runRepository.update(run,id);
+//    }
+//    @DeleteMapping("/runs/delete/{id}")
+//    void deleteRun(@RequestBody @PathVariable Integer id){
+//        runRepository.delete(id);
+//    }
 }
--- src/main/java/dev/hunter/runnerz/run/RunRepository.java
+++ src/main/java/dev/hunter/runnerz/run/RunRepository.java
@@ -1,6 +1,8 @@
 package dev.hunter.runnerz.run;
 
 import jakarta.annotation.PostConstruct;
+import org.slf4j.ILoggerFactory;
+import org.springframework.jdbc.core.simple.JdbcClient;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDateTime;
@@ -12,51 +14,11 @@ import java.util.Optional;
 
 @Repository
 public class RunRepository {
-    private List<Run> runs = new ArrayList<>();
+    private final JdbcClient jdbcClient;
 
-    List<Run> findAll() {
-        return runs;
-    }
-    Optional<Run> findByRunId(Integer id) {
-        return runs.stream()
-                .filter(run -> Objects.equals(run.id(), id))
-                .findFirst();
+    public RunRepository(JdbcClient jdbcClient) {
+        this.jdbcClient = jdbcClient;
     }
 
-    @PostConstruct
-    private void init() {
-        runs.add(new Run(1,
-                "Monday Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(1, ChronoUnit.HOURS),
-                1,
-                RunLocation.OUTDOOR));
 
-        runs.add(new Run(2,
-                "Wednesday afternoon Run",
-                LocalDateTime.now(),
-                LocalDateTime.now().plus(2, ChronoUnit.HOURS),
-                2,
-                RunLocation.INDOOR));
-
-    }
-
-    public void create(Run run) {
-        runs.add(run);
-    }
-    public void update(Run run, Integer id) {
-        Optional<Run> existingRun = findByRunId(id);
-        if (existingRun.isPresent()) {
-            runs.set(runs.indexOf(existingRun.get()), run);
-        }
-        else{
-            create(run);
-        }
-    }
-    public void delete(Integer id) {
-        Optional<Run> existingRun = findByRunId(id);
-        if (existingRun.isPresent()) {
-            runs.remove(existingRun.get());
-        }
-    }
 }
