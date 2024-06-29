fb3a9ae32c4c38c1c026f8f29eb98fe484bfd086
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-28 22:55:05 -0400

    Fixed CSS linking issue in mainscreen.html by adjusting the href path

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,11 @@
 INSERT INTO Run (id, title, started_on, completed_on, miles, location)
-VALUES (1, 'Morning RUN', '2017-01-01 06:00:00', '2017-01-01 07:00:00', 4, 'OUTDOOR');
+VALUES (1, 'Morning Run', '2017-01-01 06:00:00', '2017-01-01 07:00:00', 4, 'OUTDOOR');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (2, 'Evening Run', '2017-01-02 18:00:00', '2017-01-02 19:00:00', 5, 'OUTDOOR');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (3, 'Weekend Marathon', '2017-01-07 08:00:00', '2017-01-07 12:00:00', 26, 'OUTDOOR');
+
+INSERT INTO Run (id, title, started_on, completed_on, miles, location)
+VALUES (4, 'Indoor Treadmill Run', '2017-01-08 10:00:00', '2017-01-08 10:30:00', 3, 'INDOOR');
