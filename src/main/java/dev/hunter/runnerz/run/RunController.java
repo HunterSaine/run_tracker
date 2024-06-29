221476607be0e33520200a1f4fb71240a3066641
HunterSaine <112508289+HunterSaine@users.noreply.github.com>
2024-06-28 22:12:21 -0400

    feat: Implement HTML table view for Run data and update RunController
    This commit includes changes to the 'mainscreen.html' file to display Run data in a table format. It also updates the 'RunController.java' file to handle the GET request for the main screen and populate the model with Run data from the RunRepository. The controller has been switched from RestController to Controller to support Thymeleaf HTML views.

--- src/main/java/dev/hunter/runnerz/run/RunController.java
+++ src/main/java/dev/hunter/runnerz/run/RunController.java
@@ -1,13 +1,15 @@
 package dev.hunter.runnerz.run;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 public class RunController {
 
     private final RunRepository runRepository;
@@ -17,34 +19,46 @@ public class RunController {
     }
 
     @GetMapping
-    List<Run> findAll(){
+    List<Run> findAll() {
         return runRepository.findAll();
     }
+
     @GetMapping("/{id}")
     Run findById(@PathVariable Integer id) {
         Optional<Run> run = runRepository.findById(id);
-        if(run.isEmpty()) {
+        if (run.isEmpty()) {
             throw new ResponseStatusException(HttpStatus.NOT_FOUND, "Run not found.");
         }
         return run.get();
     }
+
     @ResponseStatus(HttpStatus.CREATED)
     @PostMapping("/runs")
-    void newRun(@RequestBody Run run){
+    void newRun(@RequestBody Run run) {
         runRepository.create(run);
     }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @PutMapping("/runs/update/{id}")
-    void update(@RequestBody Run run,@PathVariable Integer id){
-        runRepository.update(run,id);
+    void update(@RequestBody Run run, @PathVariable Integer id) {
+        runRepository.update(run, id);
     }
+
     @ResponseStatus(HttpStatus.NO_CONTENT)
     @DeleteMapping("/runs/delete/{id}")
-    void deleteRun(@RequestBody @PathVariable Integer id){
+    void deleteRun(@RequestBody @PathVariable Integer id) {
         runRepository.delete(id);
     }
+
     @GetMapping("/runs")
-    List<Run> findByLocation(@RequestParam String location){
+    List<Run> findByLocation(@RequestParam String location) {
         return runRepository.findByLocation(location);
+    }
+
+    @GetMapping("/mainscreen")
+    public String listRuns(Model theModel) {
+        List<Run> runList = runRepository.findAll();
+        theModel.addAttribute("runs", runList);
+        return "mainscreen";
     }
 }
